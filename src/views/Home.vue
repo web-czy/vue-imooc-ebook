65be63dc6b76abbd707fd8cd80247dc2ab5bcced
study-demo <1540982333@qq.com>
2019-10-18 17:59:53 +0800

    first

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <span class="iconfont icon-selected"></span>
+    <div id="read"></div>
+  </div>
+</template>
+
+<script>
+import Epub from 'epubjs'
+global.epub = Epub
+export default {
+  name: 'home',
+  mounted() {
+    this.book = new Epub('/SuperTimeFinishing.epub')
+    this.book
+      .renderTo('read', {
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+      .display()
+  }
+}
+</script>
