7518bc0e579ba3b7c89c033dd946b1381dd3b449
study-demo <1540982333@qq.com>
2019-10-21 17:49:28 +0800

    '10-21'

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,25 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <span class="text">ABCKEFG</span>
     <router-view />
   </div>
 </template>
 
-<style>
+<script type='text/ecmascript-6'>
+export default {}
+document.addEventListener('COMContentLoaded', () => {
+  const html = document.querySelector('html')
+  let fontSize = window.innerWidth / 10
+  fontSize = fontSize > 50 ? 50 : fontSize
+  html.style.fontSize = fontSize + 'px'
+})
+</script>
+
+<style scoped lang='scss'>
+.text {
+  font-family: 'Days One';
+  font-size: 20px;
+  color: orange;
+}
 </style>
